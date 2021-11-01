544731e330c55e004a3150198630977ce6c536ce
immortalprogrammist <immortalvelial@icloud.com>
2021-11-01 20:32:17 +0100

    add user elements and constrains

--- CustomBottomSheetViewControllerUIKit/ViewController.swift
+++ CustomBottomSheetViewControllerUIKit/ViewController.swift
@@ -8,9 +8,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Tim Cook"
+        label.font = .boldSystemFont(ofSize: 32)
+        return label
+    }()
 
+    lazy var descriptionTextView: UITextView = {
+        let textView = UITextView(frame: .zero)
+        textView.font = .systemFont(ofSize: 16)
+        textView.isEditable = false
+        textView.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem fringilla ut morbi tincidunt augue interdum. Ut morbi tincidunt augue interdum velit euismod in pellentesque massa. Pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a. Eget nullam non nisi est sit amet. Odio pellentesque diam volutpat commodo. Id eu nisl nunc mi ipsum faucibus vitae. "
+        return textView
+    }()
+    
+    lazy var infoButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("More info", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .tintColor
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    lazy var containerStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, descriptionTextView, infoButton])
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        setup(containerStackView)
+        setupConstraints()
+    }
+    
+    private func setup(_ subViews: UIView...) {
+        subViews.forEach { subView in
+            view.addSubview(subView)
+        }
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func setupConstraints() {
+        containerStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            containerStackView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20),
+            containerStackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -20),
+            containerStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 20),
+            containerStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20),
+            infoButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 }
 
