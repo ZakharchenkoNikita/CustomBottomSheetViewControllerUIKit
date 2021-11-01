d95e969e20245cb64239dd7b181df3d73b6f509b
immortalprogrammist <immortalvelial@icloud.com>
2021-11-01 21:53:42 +0100

    add change modalView with gestures

--- CustomBottomSheetViewControllerUIKit/ModalViewController.swift
+++ CustomBottomSheetViewControllerUIKit/ModalViewController.swift
@@ -8,9 +8,6 @@
 import UIKit
 
 class ModalViewController: UIViewController {
-
-    let maxDimmedAlpha: CGFloat = 0.5
-    let defaultHeight: CGFloat = 300
     
     lazy var containerView: UIView = {
         let view = UIView()
@@ -29,22 +26,116 @@ class ModalViewController: UIViewController {
     
     var containerViewHeightConstraint: NSLayoutConstraint?
     var containerViewBottomConstraint: NSLayoutConstraint?
+
+    let maxDimmedAlpha: CGFloat = 0.5
+    let defaultHeight: CGFloat = 300
+    let dismissHeight: CGFloat = 200
+    let maximumContainerHeight: CGFloat = UIScreen.main.bounds.height - 40
+    var currentContainerHeight: CGFloat = 300
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         setupConstrains()
+        setupPanGesture()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        animateShowDimmedView()
+        animatePresentContainer()
+    }
+    
+    private func animatePresentContainer() {
+        UIView.animate(withDuration: 0.3) { [unowned self] in
+            containerViewBottomConstraint?.constant = 0
+            view.layoutIfNeeded()
+        }
+    }
+    
+    private func animateShowDimmedView() {
+        dimmedView.alpha = 0
+        UIView.animate(withDuration: 0.4) { [unowned self] in
+            dimmedView.alpha = maxDimmedAlpha
+        }
+    }
+    
+    private func animateDismissView() {
+        UIView.animate(withDuration: 0.3) { [unowned self] in
+            containerViewBottomConstraint?.constant = defaultHeight
+            view.layoutIfNeeded()
+        }
+        
+        dimmedView.alpha = maxDimmedAlpha
+        UIView.animate(withDuration: 0.4) { [unowned self] in
+            dimmedView.alpha = 0
+        } completion: { _ in
+            self.dismiss(animated: false)
+        }
+    }
+    
+    private func setupPanGesture() {
+        let panGesture = UIPanGestureRecognizer(target: self,
+                                                action: #selector(handlePanGesture(gesture:)))
+        panGesture.delaysTouchesBegan = false
+        panGesture.delaysTouchesEnded = false
+        view.addGestureRecognizer(panGesture)
+    }
+    
+    @objc private func handlePanGesture(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: view)
+        // drag to top will be minus value and vice versa
+        print("Pan gesture y offset: \(translation.y)")
+
+        // get drag direction
+        let isDraggingDown = translation.y > 0
+        print("Dragging direction: \(isDraggingDown ? "going down" : "going up")")
+        
+        let newHeight = currentContainerHeight - translation.y
+        
+        switch gesture.state {
+        case .changed:
+            if newHeight < maximumContainerHeight {
+                containerViewHeightConstraint?.constant = newHeight
+                view.layoutIfNeeded()
+            }
+        case .ended:
+            if newHeight < dismissHeight {
+                animateDismissView()
+            }
+            else if newHeight < defaultHeight {
+                animateContainerHeight(defaultHeight)
+            }
+            else if newHeight < maximumContainerHeight && isDraggingDown {
+                animateContainerHeight(defaultHeight)
+            }
+            else if newHeight > defaultHeight && !isDraggingDown {
+                animateContainerHeight(maximumContainerHeight)
+            }
+        default:
+            break
+        }
+    }
+    
+    private func animateContainerHeight(_ height: CGFloat) {
+        UIView.animate(withDuration: 0.4) { [unowned self] in
+            containerViewHeightConstraint?.constant = height
+            view.layoutIfNeeded()
+        }
+        
+        currentContainerHeight = height
+    }
+    
     private func setupView() {
         view.backgroundColor = .clear
     }
     
     private func setupConstrains() {
-        view.addSubview(containerView)
         view.addSubview(dimmedView)
+        view.addSubview(containerView)
         
         containerView.translatesAutoresizingMaskIntoConstraints = false
-        containerView.translatesAutoresizingMaskIntoConstraints = false
+        dimmedView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             dimmedView.topAnchor.constraint(equalTo: view.topAnchor),
